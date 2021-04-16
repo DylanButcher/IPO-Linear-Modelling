f00c29da26e7aeafbf59abf1c9541d94b248c069
DylanButcher <dylanlbutcher+acc@gmail.com>
2021-04-16 19:43:07 +0100

    Further categorisation

--- December2020.R
+++ December2020.R
@@ -1,16 +1,20 @@
-library(readxl)
+library(stringr)
 
+
+#imports readxl and reads in data from IPOSHEET and sanitizes it. transpose, removes a few unused column
+library(readxl)
 december <- readxl::read_excel("IPOSHEET.xlsx",sheet="Dec2020PC",col_names = TRUE,row.names(TRUE))
 december <-t(december)
 december <- december[-c(2,3), ]
 december.names <- december[1,]
 colnames(december) <- december.names
 december <- december[-c(1),]
-
 count <- c(1:29)
 
-pdf("plottings1.pdf")
 
+
+#this is for extracting all plots to a pdf for viewing
+pdf("plottings1.pdf")
 for (x in december.names){
   plot.new()
   print(x)
@@ -20,8 +24,29 @@ for (x in december.names){
   abline(lm)
 }
 dev.off()
-  
 
+
+
+#me trying to sort pngs
+for (x in december.names){
+  print(x)
+  x.clean <- str_replace(x,"[:punct:]","")
+  fileName <- str_c(x.clean,".png")
+  png(fileName)
+  plot.new()
+  lm = lm(december[,x]~count)
+  summary(lm)
+  plot(december[,x]~count,pch=19,xlab="Days after IPO",ylab="Percentage Change",main=x)
+  abline(lm)
+  dev.off()
+  if(coef(lm)[2]<0){
+    file.rename(fileName,str_c(getwd(),"/negative/",fileName))
+  }else{
+    file.rename(fileName,str_c(getwd(),"/positive/",fileName))
+  }
+}
+
+#counts number of plots with positve and negative trends
 positiveCount = 0
 for (x in december.names){
   print(x)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # IPOModelling
+First step into my epq analysis of IPO trends over the past year. Just gone rather vanilla and have looked into December2020 and made linear regressions of each tickers progress over 30 days and sorted them by negative and positive regressions.
